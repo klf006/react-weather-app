ac6bc694113240fd3a2967ad9b946aa502cdef9b
klf006 <49726847+klf006@users.noreply.github.com>
2020-08-10 17:05:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const API = {
+  key: "cc7581bc4d1e89b910f7d21fe45199dc",
+  base: "api.openweathermap.org/data/2.5/"
+}
+
+//Date Builder for location box
+const dateBuilder = (d) =>{
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${month} ${date}, ${year}`
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+//Search callback for location
+  const search = event => {
+    if (event.key === "Enter"){
+      fetch(`${API.base}weather?q=${query}&units=imperial&APPID=${API.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+      });
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+            />
+            <div className="location-box">
+              <div className="location">Houston, TX</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                95°F
+              </div>
+              <div className="weather">
+                Sunny
+              </div>
+            </div>
+        </div>
+      </main>
     </div>
   );
 }
